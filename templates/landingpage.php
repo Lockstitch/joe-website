258c2a4203d249d883252a9cf29e8178f1e0872a
Lockstitch <48513243+Lockstitch@users.noreply.github.com>
2020-06-03 15:34:32 +0700

    my first commit

--- templates/landingpage.php
+++ templates/landingpage.php
@@ -6,6 +6,311 @@
  */
 get_header();
 ?>
-<H1> Hello World</H1>
 
+
+<?php if(wp_is_mobile()) : ?>
+  
+  <section class="hero hero-image">
+        <div class="hero-body" >
+        <div class="container hero-container">    
+
+        </div>
+    </div>
+</section>
+
+<section class="mobile-section">
+    
+     <div class = "mobile-container">
+         <h1 class="is-title is-size-1 content-header-mobile">The Kebab Movement</h1>
+         
+         <h2 class="heading-2-mobile">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium</h2>
+
+         <button class="main-button-mobile" type="button">Lets Eat!</button>
+        
+            <div class = "first-image-mobile" style="background-image: url('http://localhost/joewebsite/wp-content/uploads/2020/05/alexander-mils-SNLfVYmL8os-unsplash-1-scaled.jpg');">
+            
+            </div>
+       </div>
+          
+</section>
+
+<section class="mobile-section">
+    
+     <div class = "mobile-container">
+          <p class ="paragraph-style-mobile">Consectetur, adipisci velit, perspiciatis unde omnis iste natus error sit sed quia non numquam eius modi nisi ut aliquid adipisci velit, perspiciatis </p>
+              
+          <i class="fas fa-quote-right fa-2x first-quote-mobile"></i>
+             
+        
+            <div class = "quote-image-mobile" style="background-image: url('http://localhost/joewebsite/wp-content/uploads/2020/05/doner-kebab-png.png');">
+            
+            </div>
+       </div>
+          
+</section>
+
+<section class="mobile-section">
+    
+     <div class = "mobile-container">
+         <h1 class="is-title is-size-1 content-header-mobile" style="margin-left:80px;">Our Place </h1>
+         
+         <h2 class="heading-2-mobile">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium</h2>
+
+        
+         <video id="player-mobile" playsinline controls data-poster="/path/to/poster.jpg">
+         <source src="/path/to/video.mp4" type="video/mp4" />
+
+       </div>
+          
+</section>
+
+
+
+<div class = "image-line-mobile">
+        <img class="small-image-mobile"src="http://localhost/joewebsite/wp-content/uploads/2020/05/Webp.net-resizeimage-3.jpg" alt="">
+        <img class="small-image-mobile" src="http://localhost/joewebsite/wp-content/uploads/2020/05/Webp.net-resizeimage-7.jpg" alt="">
+        <img class="small-image-mobile" src="http://localhost/joewebsite/wp-content/uploads/2020/05/Webp.net-resizeimage-1.png" alt="">
+        <img class="small-image-mobile" src="http://localhost/joewebsite/wp-content/uploads/2020/05/Webp.net-resizeimage-8.jpg" alt="">
+        </div>    
+
+
+<section class="mobile-section">
+    
+     <div class = "mobile-container">
+          <p class ="paragraph-style-mobile">Consectetur, adipisci velit, perspiciatis unde omnis iste natus error sit sed quia non numquam eius modi nisi ut aliquid adipisci velit, perspiciatis </p>
+              
+          <i class="fas fa-quote-right fa-2x first-quote-mobile"></i>
+             
+        
+            <div class = "quote-image-mobile-2" style="background-image: url('http://localhost/joewebsite/wp-content/uploads/2020/05/kebab_PNG33.png');">
+            
+            </div>
+       </div>
+          
+</section>
+
+<section class="mobile-section">
+    
+     <div class = "mobile-container">
+         <h1 class="is-title is-size-1 content-header-mobile" style="margin-left:30px;">One stop shop</h1>
+         
+         <h2 class="heading-2-mobile">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium</h2>
+
+         <button class="main-button-mobile" type="button">Lets Eat!</button>
+        
+            <div class = "first-image-mobile" style="background-image: url('http://localhost/joewebsite/wp-content/uploads/2020/05/jez-timms-J7J0oTps4Lo-unsplash-scaled.jpg');">
+            
+            </div>
+       </div>
+          
+</section>
+
+<section class="main-section ">
+  <div class="container level">
+   <h1 class="is-size-1 level-item has-text-centered"> The Team </h1>
+  </div>
+  <div id="instafeed"></div> 
+</section>
+
+<section class="main-section-mobile ">
+  <div class="hero-body hero-small">
+    <div class=" small-hero-container-mobile">
+      <h1 class="title-mobile" style="margin-bottom: 0;">
+        subscribe and get the latest news
+      </h1>
+      <button class="main-button small-hero-button-mobile" type="button">Email</button>
+    </div>
+  </div>
+</section>
+
+
+
+<section class="mobile-section">
+    
+     <div class = "mobile-container">
+          <p class ="paragraph-style-mobile">Consectetur, adipisci velit, perspiciatis unde omnis iste natus error sit sed quia non numquam eius modi nisi ut aliquid adipisci velit, perspiciatis </p>
+              
+          <i class="fas fa-quote-right fa-2x first-quote-mobile"></i>
+             
+        
+            <div class = "quote-image-mobile-3" style="background-image: url('http://localhost/joewebsite/wp-content/uploads/2020/05/5b8ed966a639e004e1cab56b.png');">
+            
+            </div>
+       </div>
+          
+</section>
+
+
+
+<?php else :  ?>  
+
+
+
+
+
+
+<section class="hero is-fullheight hero-image">
+        <div class="hero-body" >
+        <div class="container hero-container">    
+
+        </div>
+    </div>
+</section>
+
+<section class="main-section">
+    
+     <div class = "columns columnscss">
+       <div class="column column-left-writing"> 
+         <h1 class="is-title is-size-1 content-header">The Kebab Movement</h1>
+         
+         <h2 class="heading-2">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium</h2>
+         <p class ="paragraph-style">consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? </p>
+         <button class="main-button" type="button">Products</button>
+      </div>  
+        
+
+        <div class="column column-right">
+            <div class = "first-image" style="background-image: url('http://localhost/joewebsite/wp-content/uploads/2020/05/alexander-mils-SNLfVYmL8os-unsplash-1-scaled.jpg');">
+            </div>
+              </div>
+              </div>
+          
+</section>
+
+<section class="main-section">
+    
+     <div class = "columns columnscss">
+      
+      <div class="column column-left-picture">
+            <div class = "first-png" style="background-image: url('http://localhost/joewebsite/wp-content/uploads/2020/05/doner-kebab-png.png');">
+            </div>
+            </div>
+
+       <div class="column column-right-comment"> 
+         
+         
+         <p class ="paragraph-style-big-right">Consectetur, adipisci velit, perspiciatis unde omnis iste natus error sit sed quia non numquam eius <br> modi nisi ut aliquid adipisci velit, perspiciatis </p>
+         <i class="fas fa-quote-right fa-4x first-quote"></i>
+       </div>  
+        
+
+              
+     </div>
+          
+</section>
+
+<section class="main-section">
+    
+     <div class = "columns columnscss">
+       <div class="column column-left-writing"> 
+         <h1 class="is-title is-size-1 video-header">Our Place</h1>
+         
+         <p class ="paragraph-style">consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? </p>
+      </div>  
+        
+      <video id="player" playsinline controls data-poster="/path/to/poster.jpg">
+      <source src="/path/to/video.mp4" type="video/mp4" />
+          
+
+      <!-- Captions are optional -->
+      <track kind="captions" label="English captions" src="/path/to/captions.vtt" srclang="en" default />
+      </video>
+      
+
+         
+              </div>
+        
+</section>
+
+
+        <div class = "image-line">
+        <img class="small-image"src="http://localhost/joewebsite/wp-content/uploads/2020/05/Webp.net-resizeimage-3.jpg" alt="">
+        <img class="small-image" src="http://localhost/joewebsite/wp-content/uploads/2020/05/Webp.net-resizeimage-7.jpg" alt="">
+        <img class="small-image" src="http://localhost/joewebsite/wp-content/uploads/2020/05/Webp.net-resizeimage-1.png" alt="">
+        <img class="small-image" src="http://localhost/joewebsite/wp-content/uploads/2020/05/Webp.net-resizeimage-8.jpg" alt="">
+        </div>    
+
+
+
+<section class="main-section">
+    
+     <div class = "columns columnscss">
+       <div class="column column-left-writing"> 
+         
+         <p class ="paragraph-style-big-left">consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam.  </p>
+         <i class="fas fa-quote-right fa-4x second-quote"></i>
+      </div>  
+        
+
+        <div class="column column-right">
+            <div class = "second-png" style="background-image: url('http://localhost/joewebsite/wp-content/uploads/2020/05/kebab_PNG33.png');">
+            </div>
+              </div>
+              </div>
+          
+</section>
+
+<section class="main-section">
+    
+     <div class = "columns columnscss">
+       <div class="column column-left-picture"> 
+       <div class = "first-image" style="background-image: url('http://localhost/joewebsite/wp-content/uploads/2020/05/jez-timms-J7J0oTps4Lo-unsplash-scaled.jpg');">
+            </div>
+         
+      </div>  
+        
+
+        <div class="column column-right-writing">
+        <h1 class="is-title is-size-1 content-header">One Stop Shop</h1>
+         
+         <h2 class="heading-2">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium</h2>
+         <p class ="paragraph-style">consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? </p>
+         <button class="main-button" type="button">Products</button>
+              </div>
+              </div>
+          
+</section>
+
+<section class="main-section ">
+  <div class="container level">
+   <h1 class="is-size-1 level-item has-text-centered"> The Team </h1>
+  </div>
+  <div id="instafeed"></div> 
+</section>
+
+<section class="main-section hero is-medium">
+  <div class="hero-body hero-small">
+    <div class="container small-hero-container">
+      <h1 class="title" style="margin-bottom: 0;">
+        subscribe and get the latest news
+      </h1>
+      <button class="main-button small-hero-button" type="button">Email</button>
+    </div>
+  </div>
+</section>
+
+<section class="main-section">
+    
+     <div class = "columns columnscss">
+      
+      <div class="column column-left-picture">
+            <div class = "first-png" style="background-image: url('http://localhost/joewebsite/wp-content/uploads/2020/05/5b8ed966a639e004e1cab56b.png');">
+            </div>
+            </div>
+
+       <div class="column column-right-comment"> 
+         
+         
+         <p class ="paragraph-style-big-right">Consectetur, adipisci velit, perspiciatis unde omnis iste natus error sit sed quia non numquam <br> eius modi nisi ut aliquid </p>
+         <i class="fas fa-quote-right fa-4x first-quote"></i>
+       </div>  
+        
+
+              
+     </div>
+          
+</section>
+
+</section>
+<?php endif; ?>
 <?php get_footer(); ?>
